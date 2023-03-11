e0445fe1399249b7cce10c32ab16243dcf7d1bfe
Yury Vashkevich <yury_vashkevich1@epam.com>
2023-03-11 23:11:59 +0100

    fix/ task 5 upload file.scv

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://m5lllv4o01.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://1axgx2yhe8.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://m5lllv4o01.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
